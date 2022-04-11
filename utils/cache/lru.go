797a0314eeaddba32cac128d98135f7989d5c9d9
loloxwg <loloxwg@outlook.com>
2022-04-12 02:54:07 +0800

    feat: window lru finished

--- utils/cache/lru.go
+++ utils/cache/lru.go
@@ -25,8 +25,25 @@ func newWindowLRU(size int, data map[uint64]*list.Element) *windowLRU {
 
 func (lru *windowLRU) add(newitem storeItem) (eitem storeItem, evicted bool) {
 	//implement me here!!!
+	if lru.list.Len() < lru.cap {
+		eitem = newitem
+		lru.data[eitem.key] = lru.list.PushFront(eitem)
+		return
+	}
+	//要淘汰的item 在list中的位置是链表尾部
+	evictItem := lru.list.Back()
+	item := evictItem.Value.(*storeItem)
+
+	delete(lru.data, item.key)
+	//lru.list.Remove(evictedItem)
+	eitem, *item = *item, newitem
+	//这里实现的和普通的lru不同
+	lru.data[item.key] = lru.list.PushFront(evictItem)
+	return eitem, true
 }
 
 func (lru *windowLRU) get(v *list.Element) {
 	//implement me here!!!
+	lru.list.MoveToFront(v)
+
 }
